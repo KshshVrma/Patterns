977d86b99662884663b1527c21733306ed8a11bb
Kashish <kashishverma8382@gmail.com>
2025-04-13 15:52:49 +0530

    add structure

---
+++ Patterns/src/test/java/com/Pattern/Patterns/PatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Pattern.Patterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatternsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
