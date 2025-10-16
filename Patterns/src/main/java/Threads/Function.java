f3f448b05deab06ab867d2beb18e698a7dde19e5
Kashish <kashishverma8382@gmail.com>
2025-10-16 21:31:47 +0530

    ananymous class and lambda basics

---
+++ Patterns/src/main/java/Threads/Function.java
@@ -0,0 +1,37 @@
+package Threads;
+
+class MyThread implements Runnable{
+    @Override
+    public void run() {
+        System.out.println("Thread is running");
+    }
+}
+public class Function {
+    public static void main(String[] args) {
+        Runnable myThread=new Runnable(){
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName());
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println("Thread is running");
+            }
+        };
+        Thread thread=new Thread(()->{
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName());
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println("Thread is running");
+            }
+        });
+        thread.start();
+    }
+}
